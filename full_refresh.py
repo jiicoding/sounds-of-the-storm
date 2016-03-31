f2a7a0d105db0d6f9ccc2f87e2a1b426c47d3ddc
Reed Boyer <rpboyer15@gmail.com>
2016-03-31 12:29:51 -0500

    reformatted html structure of hero pages and increased functionality of hero page generators

--- full_refresh.py
+++ full_refresh.py
@@ -28,8 +28,6 @@ for name in os.listdir('VO_Hero'):
     for skin in os.listdir('VO_Hero/' + name):
         skins = os.listdir('VO_Hero/' + name)
         if skin != '.DS_Store':
-            inc = len(skins)
-
             skin_file = skin #skin_file = name of selected skin
             skins.remove(skin_file) #Remove skin selection from 'skins' array
             link_list = [] #instantiate list for alternate skin hyperlinks
@@ -50,64 +48,8 @@ for name in os.listdir('VO_Hero'):
                     #(ex.) skin_file = Li-Ming - Star Princess
                     #          title = Star Princess Li-Ming
 
-            #There's three header possibilities
-            #1) No alternate skins
-            #2) 1 alternate skin
-            #3) 2 alternate skins
-
-            #1) No alternate skins
-            if inc == 1:
-              header = """<!DOCTYPE html>
-<html>
-<head>
-  <title>{0}</title>
-  <meta charset="utf-8">
-  <link href="../styles/skin_heroes.css" rel="stylesheet" type="text/css">
-  <link href='http://fonts.googleapis.com/css?family=Oswald:700' rel='stylesheet' type='text/css'>
-</head>
-<body>
-  <div class="header">
-    <h3 id="right"></h3>
-    <h2>{0}</h2>
-    <h3 id="left"><a href="../index.html">Main Menu</a></h3>
-  </div>
-  <img src="../images/{1}.jpg" class="icon" alt="{0}"/>
-  <br>
-  <a href="DOWNLOAD_LINK_HERE" target='_blank' class="link">
-    <div class="download">
-      <h4>Download</h4>
-    </div>
-  </a>
-  <br>""".format(title,title.lower())
-
-            #2) 1 alternate skin
-            elif inc == 2:
-              header = """<!DOCTYPE html>
-<html>
-<head>
-  <title>{0}</title>
-  <meta charset="utf-8">
-  <link href="../styles/skin_heroes.css" rel="stylesheet" type="text/css">
-  <link href='http://fonts.googleapis.com/css?family=Oswald:700' rel='stylesheet' type='text/css'>
-</head>
-<body>
-  <div class="header">
-    <h3 id="right"><a href="{1}.html">{3}</a></h3>
-    <h2>{0}</h2>
-    <h3 id="left"><a href="../index.html">Main Menu</a></h3>
-  </div>
-  <img src="../images/{2}.jpg" class="icon" alt="{0}"/>
-  <br>
-  <a href="DOWNLOAD_LINK_HERE" target='_blank' class="link">
-    <div class="download">
-      <h4>Download</h4>
-    </div>
-  </a>
-  <br>""".format(title,skins[0],title.lower(),link_list[0])
-
-            #3) 2 alternate skins
-            else:
-              header ="""<!DOCTYPE html>
+            #Write the initial lines of the html file
+            header = """<!DOCTYPE html>
 <html>
   <head>
     <title>{0}</title>
@@ -116,76 +58,119 @@ for name in os.listdir('VO_Hero'):
     <link href='http://fonts.googleapis.com/css?family=Oswald:700' rel='stylesheet' type='text/css'>
   </head>
   <body>
-    <div class="header">
-      <h3 id="right"><a href="{1}.html">{2}</a></h3>
-      <h2>{0}</h2>
-      <h3 id="left"><a href="../index.html">Main Menu</a></h3>
+    <div class="col" align="left">
+      <h3><a href="../index.html">Main Menu</a></h3>
     </div>
-    <h3 id="right"><a href="{3}.html">{4}</a></h3>
-    <p></p>
-    <h1>
-        <img src="../images/{5}.jpg" class="icon" alt="{0}"/>
-    </h1>
-    </br>
-    <a href="DOWNLOAD_LINK_HERE" target='_blank' class="link">
-      <div class="download">
-        <h4>Download</h4>
-      </div>
-    </a>
-    <br>""".format(title,skins[0],link_list[0],skins[1],link_list[1],title.lower())
+    <div class="col" align="center">
+      <h2>{0}</h2>
+      <img src="../images/{1}.jpg" alt="{0}"/><br>""".format(title,title.lower())
 
-            #Create file for hero in hero_pages folder named skin_file
-            hero_page = open("hero_pages/{}.html".format(skin_file),"w")
-            #Write header to file
-            hero_page.write(header)
+            text = header
+
+            #Get old download link
+            dlFound = False
+            for heroFile in os.listdir('hero_pages/'):
+                if heroFile != '.DS_Store':
+                    if skin_file in heroFile:
+                        #read heroFile
+                        readFile = open(os.getcwd()+'/'+'hero_pages'+'/'+skin_file+'.html')
+                        fileContent = readFile.readlines()
+                        for seg in fileContent:
+                            if 'mega.nz' in seg:
+                                downloadURL = seg[seg.index('<a href="')+8:seg.index(" target=")]
+                                dlFound = True
+                        break
+
+            #Generate html surrounding doanload link
+            if dlFound is True:
+                dl = """
+      <a href={} target='_blank' class="link">
+        <div class="download">
+          <h4>Download</h4>
+        </div>
+      </a>
+    </div>""".format(downloadURL)
+            else:
+                dl = """
+      <a href="DOWNLOAD_LINK_HERE" target='_blank' class="link">
+        <div class="download">
+          <h4>Download</h4>
+        </div>
+      </a>
+    </div>"""
+                print("Missing download link for: '" + skin_file + "'")
+            text += dl
+
+            altSkinHeader = """
+  <div class="col" align="right">
+    <h3>"""
+            text += altSkinHeader
+
+            for i in range(0, len(skins)):
+                altSkinBody = """
+        <a href="{0}.html">{1}</a><br>""".format(skins[i],link_list[i])
+                text += altSkinBody
+
+            altSkinCloser = """
+      </h3>
+    </div>
+    """
+            text += altSkinCloser
 
             #Generate mp3 body
             counter = 0
             #loop through each mp3 file in skin folder
             for track in os.listdir('VO_Hero/{}/{}'.format(name,skin_file)):
-              #ex. track = 'AnubarakCybarakBase_AI_Attack00.mp3'
-              #ignore '.DS_Store'
-              if track == '.DS_Store':
-                continue
+                #ex. track = 'AnubarakCybarakBase_AI_Attack00.mp3'
+                #ignore '.DS_Store'
+                if track == '.DS_Store':
+                  continue
 
-              start = track.index('_')+1 #ex. start = index of first letter after first '_',
-                                         #hopefully starting at the action name
-              t_name = track[start:len(track)-4] #t_name = action name without .mp3
-                                                 #ex. t_name = 'AI_Attack00'
-              t_name = t_name.replace('_',' ') #t_name = action name without '_'
-                                               #ex. 'AI Attack00'
-              #html audio block
-              block = """
-<div class="track">
-  <h4>{}</h4>
-  <audio controls>
-    <source src="../VO_Hero/{}/{}/{}" />
-  </audio>
-</div>""".format(t_name,name,skin_file,track)
-              #write block to file
-              hero_page.write(block)
-              counter += 1
+                start = track.index('_')+1 #ex. start = index of first letter after first '_',
+                                           #hopefully starting at the action name
+                t_name = track[start:len(track)-4] #t_name = action name without .mp3
+                                                   #ex. t_name = 'AI_Attack00'
+                t_name = t_name.replace('_',' ') #t_name = action name without '_'
+                                                 #ex. 'AI Attack00'
+                #html audio block
+                block = """
+    <div class="track">
+      <h4>{}</h4>
+      <audio controls>
+        <source src="../VO_Hero/{}/{}/{}" />
+      </audio>
+    </div>""".format(t_name,name,skin_file,track)
+                #add block to file input string
+                text += block
+                counter += 1
             #calculate and write number of needed placeholders
             ph_count = 5 - (counter % 4)
             ph = """
-<div class="placeholder"></div>"""
+    <div class="placeholder"></div>"""
             for i in range(ph_count):
-              hero_page.write(ph)
+                text += ph
 
             #Write google analytics code and closing tags
             closer ="""
-  <script>
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-    ga('create', 'UA-67473619-1', 'auto');
-    ga('send', 'pageview');
-  </script>
-</body>
+      ga('create', 'UA-67473619-1', 'auto');
+      ga('send', 'pageview');
+    </script>
+  </body>
 </html>"""
-            hero_page.write(closer)
+            text += closer
+
+            #Create file for hero in hero_pages folder named skin_file
+            hero_page = open("hero_pages/{}.html".format(skin_file),"w")
+
+            #Write text to file
+            hero_page.write(text)
+
             #close file
             hero_page.close()
 print('done')
--- hero_gen.py
+++ hero_gen.py
@@ -3,6 +3,7 @@ import os, sys
 #User input
 while True:
     #Enter hero name
+    print('')
     print("Hero Name: ", end='')
     #Convert to titled form,
     name = input().lower().title()
@@ -19,22 +20,26 @@ skins = os.listdir('VO_Hero/{}'.format(name.title()))
 if '.DS_Store' in skins:
     skins.remove('.DS_Store')
 
-#Print list of available skins
-inc = 1
-print('Skins for {}:'.format(name))
-for skin in skins:
-    print(str(inc) + ') ' + skin)
-    inc += 1
+if len(skins) == 1:
+    s_num = 1
 
-#Get skin selection input from user
-while True:
-    print('Choose Skin Number: ', end='')
-    s_num = int(input()) #s_num = skin selection number (1-...)
-    #Verify validity selection
-    if s_num in range(1, inc):
-        break
-    else:
-        print('Invalid number, try again')
+else:
+    #Print list of available skins if there is more than one
+    inc = 1
+    print('Skins for {}:'.format(name))
+    for skin in skins:
+        print(str(inc) + ') ' + skin)
+        inc += 1
+
+    #Get skin selection input from user
+    while True:
+        print('Choose Skin Number: ', end='')
+        s_num = int(input()) #s_num = skin selection number (1-...)
+        #Verify validity selection
+        if s_num in range(1, inc):
+            break
+        else:
+            print('Invalid number, try again')
 
 skin_file = skins[s_num-1] #skin_file = name of selected skin
 skins.remove(skin_file) #Remove skin selection from 'skins' array
@@ -51,63 +56,8 @@ if 'Default' in skin_file:
 else:
     title = skin_file[skin_file.index('-')+2:] + ' ' + name #title = skin name
 
-#There's three header possibilities
-#1) No alternate skins
-#2) 1 alternate skin
-#3) 2 alternate skins
-inc = inc - 1
-
-#1) No alternate skins
-if inc == 1:
-  header = """<!DOCTYPE html>
-  <html>
-    <head>
-      <title>{0}</title>
-      <meta charset="utf-8">
-      <link href="../styles/skin_heroes.css" rel="stylesheet" type="text/css">
-      <link href='http://fonts.googleapis.com/css?family=Oswald:700' rel='stylesheet' type='text/css'>
-    </head>
-    <body>
-      <div class="header">
-        <h3 id="right"></h3>
-        <h2>{0}</h2>
-        <h3 id="left"><a href="../index.html">Main Menu</a></h3>
-      </div>
-      <img src="../images/{1}.jpg" class="icon" alt="{0}"/>
-      <br>
-      <a href="DOWNLOAD_LINK_HERE" target='_blank' class="link">
-        <div class="download">
-          <h4>Download</h4>
-        </div>
-      </a>
-      <br>""".format(title,title.lower())
-#2) 1 alternate skin
-elif inc == 2:
-  header = """<!DOCTYPE html>
-  <html>
-    <head>
-      <title>{0}</title>
-      <meta charset="utf-8">
-      <link href="../styles/skin_heroes.css" rel="stylesheet" type="text/css">
-      <link href='http://fonts.googleapis.com/css?family=Oswald:700' rel='stylesheet' type='text/css'>
-    </head>
-    <body>
-      <div class="header">
-        <h3 id="right"><a href="{1}.html">{3}</a></h3>
-        <h2>{0}</h2>
-        <h3 id="left"><a href="../index.html">Main Menu</a></h3>
-      </div>
-      <img src="../images/{2}.jpg" class="icon" alt="{0}"/>
-      <br>
-      <a href="DOWNLOAD_LINK_HERE" target='_blank' class="link">
-        <div class="download">
-          <h4>Download</h4>
-        </div>
-      </a>
-      <br>""".format(title,skins[0],title.lower(),link_list[0])
-#3) 2 alternate skins
-else:
-  header ="""<!DOCTYPE html>
+#Write the initial lines of the html file
+header = """<!DOCTYPE html>
 <html>
   <head>
     <title>{0}</title>
@@ -116,75 +66,120 @@ else:
     <link href='http://fonts.googleapis.com/css?family=Oswald:700' rel='stylesheet' type='text/css'>
   </head>
   <body>
-    <div class="header">
-      <h3 id="right"><a href="{1}.html">{2}</a></h3>
-      <h2>{0}</h2>
-      <h3 id="left"><a href="../index.html">Main Menu</a></h3>
+    <div class="col" align="left">
+      <h3><a href="../index.html">Main Menu</a></h3>
     </div>
-    <h3 id="right"><a href="{3}.html">{4}</a></h3>
-    <p></p>
-    <h1>
-        <img src="../images/{5}.jpg" class="icon" alt="{0}"/>
-    </h1>
-    </br>
-    <a href="DOWNLOAD_LINK_HERE" target='_blank' class="link">
-      <div class="download">
-        <h4>Download</h4>
-      </div>
-    </a>
-    <br>""".format(title,skins[0],link_list[0],skins[1],link_list[1],title.lower())
+    <div class="col" align="center">
+      <h2>{0}</h2>
+      <img src="../images/{1}.jpg" alt="{0}"/><br>""".format(title,title.lower())
 
-#Create file for hero in hero_pages folder named skin_file
-hero_page = open("hero_pages/{}.html".format(skin_file),"w")
-#Write header to file
-hero_page.write(header)
+text = header
+
+#Get old download link
+dlFound = False
+for heroFile in os.listdir('hero_pages/'):
+    if heroFile != '.DS_Store':
+        if skin_file in heroFile:
+            #read heroFile
+            readFile = open(os.getcwd()+'/'+'hero_pages'+'/'+skin_file+'.html')
+            fileContent = readFile.readlines()
+            for seg in fileContent:
+                if 'mega.nz' in seg:
+                    downloadURL = seg[seg.index('<a href="')+8:seg.index(" target=")]
+                    dlFound = True
+            break
+
+#Generate html surrounding doanload link
+if dlFound is True:
+    dl = """
+      <a href={} target='_blank' class="link">
+        <div class="download">
+          <h4>Download</h4>
+        </div>
+      </a>
+    </div>""".format(downloadURL)
+else:
+    dl = """
+      <a href=DOWNLOAD_LINK_HERE target='_blank' class="link">
+        <div class="download">
+          <h4>Download</h4>
+        </div>
+      </a>
+    </div>"""
+    print('')
+    print("Missing download link for: '" + skin_file + "'")
+text += dl
+
+altSkinHeader = """
+    <div class="col" align="right">
+      <h3>"""
+text += altSkinHeader
+
+for i in range(0, len(skins)):
+    altSkinBody = """
+        <a href="{0}.html">{1}</a><br>""".format(skins[i],link_list[i])
+    text += altSkinBody
+
+altSkinCloser = """
+      </h3>
+    </div>
+    """
+text += altSkinCloser
 
 #Generate mp3 body
 counter = 0
 #loop through each mp3 file in skin folder
 for track in os.listdir('VO_Hero/{}/{}'.format(name,skin_file)):
-  #ex. track = 'AnubarakCybarakBase_AI_Attack00.mp3'
-  #ignore '.DS_Store'
-  if track == '.DS_Store':
-    continue
+    #ex. track = 'AnubarakCybarakBase_AI_Attack00.mp3'
+    #ignore '.DS_Store'
+    if track == '.DS_Store':
+      continue
 
-  start = track.index('_')+1 #ex. start = index of first letter after first '_',
-                             #hopefully starting at the action name
-  t_name = track[start:len(track)-4] #t_name = action name without .mp3
-                                     #ex. t_name = 'AI_Attack00'
-  t_name = t_name.replace('_',' ') #t_name = action name without '_'
-                                   #ex. 'AI Attack00'
-  #html audio block
-  block = """
+    start = track.index('_')+1 #ex. start = index of first letter after first '_',
+                               #hopefully starting at the action name
+    t_name = track[start:len(track)-4] #t_name = action name without .mp3
+                                       #ex. t_name = 'AI_Attack00'
+    t_name = t_name.replace('_',' ') #t_name = action name without '_'
+                                     #ex. 'AI Attack00'
+    #html audio block
+    block = """
     <div class="track">
       <h4>{}</h4>
       <audio controls>
         <source src="../VO_Hero/{}/{}/{}" />
       </audio>
     </div>""".format(t_name,name,skin_file,track)
-  #write block to file
-  hero_page.write(block)
-  counter += 1
+    #add block to file input string
+    text += block
+    counter += 1
 #calculate and write number of needed placeholders
 ph_count = 5 - (counter % 4)
 ph = """
     <div class="placeholder"></div>"""
 for i in range(ph_count):
-  hero_page.write(ph)
+    text += ph
 
 #Write google analytics code and closing tags
 closer ="""
-      <script>
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-        ga('create', 'UA-67473619-1', 'auto');
-        ga('send', 'pageview');
-      </script>
-    </body>
-  </html>"""
-hero_page.write(closer)
+      ga('create', 'UA-67473619-1', 'auto');
+      ga('send', 'pageview');
+    </script>
+  </body>
+</html>"""
+text += closer
+
+#Create file for hero in hero_pages folder named skin_file
+hero_page = open("hero_pages/{}.html".format(skin_file),"w")
+
+#Write text to file
+hero_page.write(text)
+
 #close file
 hero_page.close()
+print('done')
